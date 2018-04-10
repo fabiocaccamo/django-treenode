41afb0be8d9bbd02407bf5760621cbaaf575201d
Fabio Caccamo <fabio.caccamo@gmail.com>
2018-04-10 15:07:33 +0200

    Added tests

---
+++ tests/tests.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase
+
+
+class TreeNodeTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_treenode(self):
+        self.assertTrue(True)
