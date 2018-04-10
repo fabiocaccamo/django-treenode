49ae4f385519f0c4073baf58add393579bf90e22
Fabio Caccamo <fabio.caccamo@gmail.com>
2018-04-10 15:08:30 +0200

    Added pypi setup files

---
+++ setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import os
+
+exec(open('treenode/version.py').read())
+
+github_url = 'https://github.com/fabiocaccamo'
+package_name = 'django-treenode'
+package_path = os.path.abspath(os.path.dirname(__file__))
+long_description_file_path = os.path.join(package_path, 'README.rst')
+long_description = ''
+try:
+    with open(long_description_file_path) as f:
+        long_description = f.read()
+except IOError:
+    pass
+
+setup(
+    name=package_name,
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    version=__version__,
+    description='django-treenode is an abstract model for your tree based stuff.',
+    long_description=long_description,
+    author='Fabio Caccamo',
+    author_email='fabio.caccamo@gmail.com',
+    url='%s/%s' % (github_url, package_name, ),
+    download_url='%s/%s/archive/%s.tar.gz' % (github_url, package_name, __version__, ),
+    keywords=['django', 'tree', 'node', 'categories', 'abstract', 'model', 'python', 'category', 'trees', 'nodes', 'parents', 'children', 'siblings'],
+    requires=['django(>=1.7)'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    license='MIT',
+    test_suite='runtests.runtests'
+)
