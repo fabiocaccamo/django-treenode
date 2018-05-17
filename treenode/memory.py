b0696ea4f3e38676397f167737b08f9c37c1c137
Fabio Caccamo <fabio.caccamo@gmail.com>
2018-05-17 22:37:20 +0200

    Removed old commented code [ci skip]

--- treenode/memory.py
+++ treenode/memory.py
@@ -13,34 +13,9 @@ def clear_refs(cls):
 
 
 def get_refs(cls):
-    # print(len(__refs__[cls]))
     return __refs__[cls]
 
 
 def set_ref(cls, obj):
     if obj.pk:
         __refs__[cls].add(obj)
-
-
-# __refs__ = defaultdict(list)
-
-
-# def clear_refs(cls):
-#     __refs__[cls][:] = []
-
-
-# def get_refs(cls):
-#     refs = []
-#     for ref in __refs__[cls]:
-#         obj = ref()
-#         if obj is not None:
-#             refs.append(ref)
-#             yield obj
-#     # print(len(refs))
-#     __refs__[cls] = refs
-
-
-# def set_ref(cls, obj):
-#     if obj.pk:
-#         ref = weakref.ref(obj)
-#         __refs__[cls].append(ref)
