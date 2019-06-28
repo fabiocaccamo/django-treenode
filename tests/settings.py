8869fa7de8caafcb32a9b713c950336ea50131c9
Fabio Caccamo <fabio.caccamo@gmail.com>
2019-06-28 12:19:44 +0200

    Added django 2.2 compatibility. #15

--- tests/settings.py
+++ tests/settings.py
@@ -43,9 +43,31 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.messages',
     'django.contrib.sessions',
     'tests',
     'treenode',
+]
+
+MIDDLEWARE = [
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'debug': DEBUG,
+        },
+    },
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'treenode/public/media/')
