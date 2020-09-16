3fe379d9f4423cb125f9e272978e2ed685570a78
Fabio Caccamo <fabio.caccamo@gmail.com>
2020-09-16 15:56:38 +0200

    Added custom cache back-end support. #19 #24

--- treenode/cache.py
+++ treenode/cache.py
@@ -2,29 +2,60 @@
 
 from collections import defaultdict
 
+from django.conf import settings
+from django.core.cache import cache, caches
+
 from .utils import split_pks
 
 
-__list__ = defaultdict(list)
-__dict__ = defaultdict(dict)
+def _get_cache():
+    return caches['treenode'] \
+        if 'treenode' in settings.CACHES else cache
+
+
+def _get_cached_collection(key, dict_cls):
+    c = _get_cache()
+    value = c.get(key, None)
+    if value is None:
+        value = defaultdict(dict_cls)
+        c.set(key, value)
+    return value
+
+
+def _get_cached_collections():
+    l = _get_cached_collection('treenode_list', list)
+    d = _get_cached_collection('treenode_dict', dict)
+    return (l, d, )
+
+
+def _set_cached_collections(l, d):
+    c = _get_cache()
+    c.set('treenode_list', l)
+    c.set('treenode_dict', d)
 
 
 def clear_cache(cls):
-    del __list__[cls][:]
-    __dict__[cls].clear()
+    l, d = _get_cached_collections()
+    del l[cls][:]
+    d[cls].clear()
+    _set_cached_collections(l, d)
 
 
 def query_cache(cls, pk=None, pks=None):
-    if not __list__[cls] or not __dict__[cls]:
+    l, d = _get_cached_collections()
+    if not l[cls] or not d[cls]:
         update_cache(cls)
+        l, d = _get_cached_collections()
     if pk != None:
-        return __dict__[cls].get(str(pk))
+        return d[cls].get(str(pk))
     elif pks != None:
-        return [__dict__[cls].get(str(pk)) for pk in split_pks(pks)]
+        return [d[cls].get(str(pk)) for pk in split_pks(pks)]
     else:
-        return list(__list__[cls])
+        return list(l[cls])
 
 
 def update_cache(cls):
-    __list__[cls] = list(cls.objects.all())
-    __dict__[cls] = {str(obj.pk):obj for obj in __list__[cls]}
+    l, d = _get_cached_collections()
+    l[cls] = list(cls.objects.all())
+    d[cls] = {str(obj.pk):obj for obj in l[cls]}
+    _set_cached_collections(l, d)
