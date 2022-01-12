26d4d3c45907a88e63b76f0c386de2ff6a3f1f0a
Fabio Caccamo <fabio.caccamo@gmail.com>
2022-01-12 02:17:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,70 +5,97 @@ from setuptools import setup, find_packages
 
 import os, sys
 
-exec(open('treenode/version.py').read())
+exec(open("treenode/version.py").read())
 
-github_url = 'https://github.com/fabiocaccamo'
-package_name = 'django-treenode'
-package_url = '{}/{}'.format(github_url, package_name)
+github_url = "https://github.com/fabiocaccamo"
+sponsor_url = "https://github.com/sponsors/fabiocaccamo/"
+twitter_url = "https://twitter.com/fabiocaccamo"
+package_name = "django-treenode"
+package_url = "{}/{}".format(github_url, package_name)
+package_issues_url = "{}/issues".format(github_url)
 package_path = os.path.abspath(os.path.dirname(__file__))
-long_description_file_path = os.path.join(package_path, 'README.md')
-long_description_content_type = 'text/markdown'
-long_description = ''
+long_description_file_path = os.path.join(package_path, "README.md")
+long_description_content_type = "text/markdown"
+long_description = ""
 try:
-    long_description_file_options = {} if sys.version_info[0] < 3 else { 'encoding':'utf-8' }
-    with open(long_description_file_path, 'r', **long_description_file_options) as f:
+    long_description_file_options = (
+        {} if sys.version_info[0] < 3 else {"encoding": "utf-8"}
+    )
+    with open(long_description_file_path, "r", **long_description_file_options) as f:
         long_description = f.read()
 except IOError:
     pass
 
 setup(
     name=package_name,
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     include_package_data=True,
     version=__version__,
-    description='django-treenode is probably the best abstract model / admin for your tree based stuff.',
+    description="django-treenode is probably the best abstract model / admin for your tree based stuff.",
     long_description=long_description,
     long_description_content_type=long_description_content_type,
-    author='Fabio Caccamo',
-    author_email='fabio.caccamo@gmail.com',
+    author="Fabio Caccamo",
+    author_email="fabio.caccamo@gmail.com",
     url=package_url,
-    download_url='{}/archive/{}.tar.gz'.format(package_url, __version__),
-    keywords=['python', 'django', 'trees', 'tree', 'nodes', 'node', 'categories', 'category', 'ancestors', 'parents', 'children', 'descendants', 'siblings', 'abstract', 'model'],
-    requires=['django(>=1.8)'],
+    download_url="{}/archive/{}.tar.gz".format(package_url, __version__),
+    project_urls={
+        "Documentation": package_url,
+        "Issues": package_issues_url,
+        "Funding": sponsor_url,
+        "Twitter": twitter_url,
+    },
+    keywords=[
+        "python",
+        "django",
+        "trees",
+        "tree",
+        "nodes",
+        "node",
+        "categories",
+        "category",
+        "ancestors",
+        "parents",
+        "children",
+        "descendants",
+        "siblings",
+        "abstract",
+        "model",
+    ],
+    requires=["django(>=1.8)"],
     install_requires=[
-        'six >= 1.9.0, < 2.0.0',
+        "six >= 1.9.0, < 2.0.0",
     ],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.9',
-        'Framework :: Django :: 1.10',
-        'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.0',
-        'Framework :: Django :: 2.1',
-        'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
-        'Framework :: Django :: 3.1',
-        'Framework :: Django :: 3.2',
-        'Framework :: Django :: 4.0',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Software Development :: Build Tools',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 1.8",
+        "Framework :: Django :: 1.9",
+        "Framework :: Django :: 1.10",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Build Tools",
     ],
-    license='MIT',
-    test_suite='runtests.runtests'
+    license="MIT",
+    test_suite="runtests.runtests",
 )
