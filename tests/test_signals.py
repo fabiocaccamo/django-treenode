3b9b1f99264266f642bef53633b2dfad88fbb313
Fabio Caccamo <fabio.caccamo@gmail.com>
2025-04-30 11:01:03 +0200

    Fix tests errors.

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -1,6 +1,5 @@
 from contextlib import contextmanager
 
-from django.apps import apps
 from django.core.management.sql import emit_post_migrate_signal
 from django.db import OperationalError, ProgrammingError, connection
 from django.test import TestCase
@@ -16,23 +15,25 @@ class TreeNodeDropTableTestCase(TestCase):
         try:
             yield None
         except exc_type as e:
-            raise self.failureException(f"{e} raised")
+            raise self.failureException(f"{e} raised") from e
 
     def test_destroy_model(self):
         table_name = ModelToBeDestroyed._meta.db_table
-        app_label = ModelToBeDestroyed._meta.app_label
-        app_config = apps.get_app_config(app_label)
+        # app_label = ModelToBeDestroyed._meta.app_label
+        # app_config = apps.get_app_config(app_label)
 
-        # Drop the table (as if by a `migrate app zero`)
+        # drop the table (as if by a `migrate app zero`)
         with connection.cursor() as cursor:
             cursor.execute(f"DROP TABLE {table_name}")
 
-        # Try to use the model - this should raise an error
+        # try to use the model - this should raise an error
         with self.assertRaises((OperationalError, ProgrammingError)):
             list(ModelToBeDestroyed.objects.all())
 
-        # Emit a post_migrate signal as done after migration to zero
+        # emit a post_migrate signal as done after migration to zero
         with self.assertNotRaises(OperationalError):
             emit_post_migrate_signal(
-                verbosity=1, interactive=False, db=connection.alias
+                verbosity=1,
+                interactive=False,
+                db=connection.alias,
             )
