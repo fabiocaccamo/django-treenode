03f98b58f4ff3d7ed31017b3513255a59bdfba12
Fabio Caccamo <fabio.caccamo@gmail.com>
2025-04-30 12:35:48 +0200

    Fix tests with Postgres db.

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -2,25 +2,23 @@ from contextlib import contextmanager
 
 from django.core.management.sql import emit_post_migrate_signal
 from django.db import OperationalError, ProgrammingError, connection
-from django.test import TestCase
+from django.test import TransactionTestCase
 
 from tests.models import Category
 
 ModelToBeDestroyed = Category
 
 
-class TreeNodeDropTableTestCase(TestCase):
+class TreeNodeDropTableTestCase(TransactionTestCase):
     @contextmanager
     def assertNotRaises(self, exc_type):
         try:
             yield None
-        except exc_type as e:
-            raise self.failureException(f"{e} raised") from e
+        except exc_type as error:
+            raise self.failureException(f"{error} raised") from error
 
     def test_destroy_model(self):
         table_name = ModelToBeDestroyed._meta.db_table
-        # app_label = ModelToBeDestroyed._meta.app_label
-        # app_config = apps.get_app_config(app_label)
 
         # drop the table (as if by a `migrate app zero`)
         with connection.cursor() as cursor:
@@ -29,9 +27,6 @@ class TreeNodeDropTableTestCase(TestCase):
         # try to use the model - this should raise an error
         with self.assertRaises((OperationalError, ProgrammingError)):
             list(ModelToBeDestroyed.objects.all())
-
-        # clear the failed transaction state before emitting post_migrate
-        connection.rollback()
 
         # emit a post_migrate signal as done after migration to zero
         with self.assertNotRaises((OperationalError, ProgrammingError)):
